c3ca24d2e708613c719b2b35a850373e48a0fb2e
Krisztian Horvath <keyki.kk@gmail.com>
2019-10-10 13:56:33 +0200

    set cert validation time to 825 days

--- tlsutils.go
+++ tlsutils.go
@@ -91,7 +91,7 @@ func newCertificate(org, commonName string) (*x509.Certificate, error) {
 	// need to set notBefore slightly in the past to account for time
 	// skew in the VMs otherwise the certs sometimes are not yet valid
 	notBefore := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute()-5, 0, 0, time.Local)
-	notAfter := notBefore.Add(time.Hour * 24 * 1080)
+	notAfter := notBefore.Add(time.Hour * 24 * 825)
 
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
